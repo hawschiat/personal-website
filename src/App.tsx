0b2b6e986d1fb6228257e18399cb180f3c823fe2
SC Haw <haw@neverguess.ca>
2020-07-19 17:41:06 -0400

    lazy load pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { AnimatePresence } from "framer-motion";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Experience from "./pages/Experience";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
 import "./styles/App.css";
+import Loader from "./components/TRexLoader";
+
+const HomePage = lazy(() => import("./pages/Home"));
+const AboutPage = lazy(() => import("./pages/About"));
+const ExperiencePage = lazy(() => import("./pages/Experience"));
+const ProjectsPage = lazy(() => import("./pages/Projects"));
+const ContactPage = lazy(() => import("./pages/Contact"));
 
 export default () => {
   return (
-    <Router>
-      <div className="App">
-        <Route
-          render={({ location }) => (
-            <AnimatePresence exitBeforeEnter initial={false}>
-              <Switch location={location} key={location.pathname}>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/experience" component={Experience} />
-                <Route exact path="/projects" component={Projects} />
-                <Route exact path="/contact" component={Contact} />
-              </Switch>
-            </AnimatePresence>
-          )}
-        />
-      </div>
-    </Router>
+    <Suspense fallback={Loader}>
+      <Router>
+        <div className="App">
+          <Route
+            render={({ location }) => (
+              <AnimatePresence exitBeforeEnter initial={false}>
+                <Switch location={location} key={location.pathname}>
+                  <Route exact path="/" component={HomePage} />
+                  <Route exact path="/about" component={AboutPage} />
+                  <Route exact path="/experience" component={ExperiencePage} />
+                  <Route exact path="/projects" component={ProjectsPage} />
+                  <Route exact path="/contact" component={ContactPage} />
+                </Switch>
+              </AnimatePresence>
+            )}
+          />
+        </div>
+      </Router>
+    </Suspense>
   );
 };
