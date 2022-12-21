06503e1b988c0f429d318c4ea30109405897df5d
SC Haw <haw1997@hotmail.com>
2022-12-21 18:23:49 -0500

    Converted project page to use hooks

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactGA from "react-ga";
 import { motion } from "framer-motion";
 
@@ -15,12 +15,10 @@ type ProjectData = {
   description: string;
 };
 
-export default class ProjectPage extends React.Component {
-  state = {
-    chosen: -1,
-  };
+export default function ProjectPage() {
+  const [chosen, setChosen] = useState(-1);
 
-  chooseProject = (index: number) => {
+  const chooseProject = (index: number) => {
     if (index >= 0) {
       ReactGA.event({
         category: "Project",
@@ -28,89 +26,83 @@ export default class ProjectPage extends React.Component {
         value: index + 1,
       });
     }
-    this.setState({
-      chosen: index,
-    });
+    setChosen(index);
   };
 
-  render() {
+  useEffect(() => {
     ReactGA.pageview("/projects");
+  }, []);
 
-    return (
-      <div id="page">
-        <motion.div
-          id="back-btn-container"
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          variants={FadeVariants}
-        >
-          <AnimatedLink
-            fontAwesomeIcon={{
-              iconClass: "fas fa-arrow-left",
-              placement: "front",
-            }}
-            content="Experience"
-            to="/experience"
-          />
-        </motion.div>
-        <motion.div
-          id="continue-btn-container"
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          variants={FadeVariants}
-        >
-          <AnimatedLink
-            fontAwesomeIcon={{
-              iconClass: "fas fa-arrow-right",
-              placement: "end",
-            }}
-            content="Contact Me"
-            to="/contact"
-          />
-        </motion.div>
-        <motion.div
-          id="content-container"
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          variants={{
-            enter: { transition: { delayChildren: 0.7, staggerChildren: 0.1 } },
-            exit: {
-              transition: { staggerChildren: 0.1, staggerDirection: -1 },
-            },
+  return (
+    <div id="page">
+      <motion.div
+        id="back-btn-container"
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        variants={FadeVariants}
+      >
+        <AnimatedLink
+          fontAwesomeIcon={{
+            iconClass: "fas fa-arrow-left",
+            placement: "front",
           }}
-        >
-          <motion.div id="content-title" variants={SlideVariants}>
-            Projects
-          </motion.div>
-          <motion.p>
-            These are the projects I've done throughout my student / professional career that are publicly available.
-          </motion.p>
-          <div id="project-gallery">
-            {ProjectList
-              ? ProjectList.map((project: ProjectData, i: number) => (
-                  <GalleryItem
-                    title={project.title}
-                    imagePath={project.imagePath ?? '/images/projects/fallback.jpg'}
-                    description={project.description}
-                    link={project.link}
-                    isActive={this.state.chosen === i}
-                    key={i}
-                    onSelect={(e: React.MouseEvent) => {
-                      this.chooseProject(i);
-                    }}
-                    onCancel={(e: React.MouseEvent) => {
-                      e.stopPropagation();
-                      this.chooseProject(-1);
-                    }}
-                  />
-                ))
-              : null}
-          </div>
+          content="Experience"
+          to="/experience"
+        />
+      </motion.div>
+      <motion.div
+        id="continue-btn-container"
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        variants={FadeVariants}
+      >
+        <AnimatedLink
+          fontAwesomeIcon={{
+            iconClass: "fas fa-arrow-right",
+            placement: "end",
+          }}
+          content="Contact Me"
+          to="/contact"
+        />
+      </motion.div>
+      <motion.div
+        id="content-container"
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        variants={{
+          enter: { transition: { delayChildren: 0.7, staggerChildren: 0.1 } },
+          exit: {
+            transition: { staggerChildren: 0.1, staggerDirection: -1 },
+          },
+        }}
+      >
+        <motion.div id="content-title" variants={SlideVariants}>
+          Projects
         </motion.div>
-      </div>
-    );
-  }
+        <motion.p>
+          These are the projects I've done throughout my student / professional career that are publicly available.
+        </motion.p>
+        <div id="project-gallery">
+          {ProjectList?.map((project: ProjectData, i: number) => (
+            <GalleryItem
+              title={project.title}
+              imagePath={project.imagePath ?? '/images/projects/fallback.jpg'}
+              description={project.description}
+              link={project.link}
+              isActive={chosen === i}
+              key={i}
+              onSelect={() => chooseProject(i)}
+              onCancel={(e: React.MouseEvent) => {
+                e.stopPropagation();
+                chooseProject(-1);
+              }}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </div>
+  );
 }
